ae8e14aded316079563caa7adda2305e48adb4f0
xins Bin <q25a25q@live.com>
2016-01-29 17:23:33 +0800

    添加分页模型基类

---
+++ src/PagedList/BasePageableModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PagedList
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BasePageableModel : IPageableModel
+    {
+        #region Methods
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PageIndex
+        {
+            get
+            {
+                if (PageNumber > 0)
+                    return PageNumber - 1;
+
+                return 0;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PageNumber { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public int PageSize { get; set; }
+
+        #endregion
+    }
+}
